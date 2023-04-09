127b6b04b1280552b11ecf02c4fa5cad4131226d
KeyframeEditor <andhika.rahmanu@mail.ugm.ac.id>
2023-04-10 03:03:50 +0700

    added listview & map (again)

--- app/src/main/java/com/example/uts_shock_berat/MainActivity.java
+++ app/src/main/java/com/example/uts_shock_berat/MainActivity.java
@@ -4,18 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listview_resto;
     private Button button;
+
+    String listTest[] = {"canon","ballz","in","da","jaw"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listview_resto = findViewById(R.id.listview_resto);
         button = findViewById(R.id.button);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String> (this, R.layout.activity_listview_resto, R.id.listview_textview, listTest);
+        listview_resto.setAdapter(arrayAdapter);
+
+        listview_resto.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.i("i: ", String.valueOf(i));
+                Log.i("itemName: ", String.valueOf(listTest[i]));
+            }
+        });
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
---
+++ app/src/main/java/com/example/uts_shock_berat/listview_resto.java
@@ -0,0 +1,14 @@
+package com.example.uts_shock_berat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listview_resto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listview_resto);
+    }
+}
